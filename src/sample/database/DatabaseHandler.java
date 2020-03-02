570bba70b1ae1d6f48dc77b9d7edca53d76913d8
Maxim <githubmax@ukr.net>
2020-03-02 19:22:12 +0300

    add file DatabaseHandler.java

--- src/sample/database/DatabaseHandler.java
+++ src/sample/database/DatabaseHandler.java
@@ -1,4 +1,19 @@
 package sample.database;
 
-public class DatabaseHandler {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseHandler extends Config {
+    Connection dbConnection;
+
+    public Connection getDbConnection() throws ClassNotFoundException, SQLException {
+        String connectionString = "jdbc:mysql//" + dbHost + ":" + dbPort + "/" + dbName;
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        dbConnection = DriverManager.getConnection(connectionString, dbUser, dbPass);
+
+        return dbConnection;
+    }
 }
