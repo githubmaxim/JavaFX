81ca6f3da0bc01d53420ba4c781c2ca342d0fb88
Maxim <githubmax@ukr.net>
2020-03-02 19:19:45 +0300

    add file Config.java

--- src/sample/database/Config.java
+++ src/sample/database/Config.java
@@ -1,4 +1,9 @@
 package sample.database;
 
 public class Config {
+    protected String dbHost = "localhost";
+    protected String dbPort = "3306";
+    protected String dbUser = "root";
+    protected String dbPass = "qwa12345"; //password sql database
+    protected String dbName = "JavaFX"; //login sql database
 }
