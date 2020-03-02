266b6966e9a20886e00c6b1b20ba6397fcfacf1b
Maxim <githubmax@ukr.net>
2020-03-02 19:20:53 +0300

    add file Const.java

--- src/sample/database/Const.java
+++ src/sample/database/Const.java
@@ -1,4 +1,14 @@
 package sample.database;
 
 public class Const {
+    public static final String USER_TABLE = "users"; //table name in my database
+
+    //constant with name columns table in my database
+    public static final String USER_ID = "idusers";
+    public static final String USER_FIRSTNAME = "firstname";
+    public static final String USER_LASTNAME = "lastname";
+    public static final String USER_USERNAME = "username";
+    public static final String USER_PASSWORD = "password";
+    public static final String USER_LOCATION = "location";
+    public static final String USER_GENDER = "gender";
 }
