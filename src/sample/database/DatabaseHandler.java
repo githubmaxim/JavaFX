c4623f1d18c71b60966016a992ee55cbffa3f767
Maxim <githubmax@ukr.net>
2020-03-10 13:18:06 +0200

    add import java.sql.ResultSet

--- src/sample/database/DatabaseHandler.java
+++ src/sample/database/DatabaseHandler.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 
 public class DatabaseHandler extends Config {
     private static final Logger log = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
