1ed0b24ae01bd0cb614793ab2cb0badab1e5fe7d
Maxim <githubmax@ukr.net>
2020-03-10 12:05:33 +0200

    add code for connected to DataBase, request to insert, log4j

--- src/sample/database/DatabaseHandler.java
+++ src/sample/database/DatabaseHandler.java
@@ -1,19 +1,56 @@
 package sample.database;
 
+import org.apache.log4j.Logger;
+import sample.user.User;
+
+import java.lang.invoke.MethodHandles;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseHandler extends Config {
+    private static final Logger log = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
+
     Connection dbConnection;
 
     public Connection getDbConnection() throws ClassNotFoundException, SQLException {
-        String connectionString = "jdbc:mysql//" + dbHost + ":" + dbPort + "/" + dbName;
+        //формируем URL
+        String connectionString = "jdbc:mysql://" + dbHost + ":" + dbPort + "/" + dbName + "?" + dbTimezone;
 
-        Class.forName("com.mysql.jdbc.Driver");
+        //загружаем драйвер БД "DriverManager"
+        Class.forName("com.mysql.cj.jdbc.Driver");
 
+        //подключаемся к БД
         dbConnection = DriverManager.getConnection(connectionString, dbUser, dbPass);
 
         return dbConnection;
+    }
+
+    //метод формирующий SQL запрос, который записывает данные пользователя в поля таблицы БД
+    public void signUpUser(User user) {
+        String insert = "INSERT INTO" + " " + Const.USER_TABLE + "(" +
+                Const.USER_FIRSTNAME + "," +Const.USER_LASTNAME + "," +
+                Const.USER_PASSWORD + "," + Const.USER_USERNAME + "," +
+                Const.USER_LOCATION + "," + Const.USER_GENDER + ")" +
+                "VALUES(?,?,?,?,?,?)";
+
+        try {
+            PreparedStatement prSt = getDbConnection().prepareStatement(insert);
+            prSt.setString(1, user.getFirstName());
+            prSt.setString(2, user.getLastName());
+            prSt.setString(3, user.getUserName());
+            prSt.setString(4, user.getPassword());
+            prSt.setString(5, user.getLocation());
+            prSt.setString(6, user.getGender());
+
+            prSt.executeUpdate();
+        } catch (ClassNotFoundException e) {
+            log.error("ClassNotFoundException", e);
+            e.printStackTrace();
+        } catch (SQLException e) {
+            log.error("SQLException", e);
+            e.printStackTrace();
+        }
     }
 }
