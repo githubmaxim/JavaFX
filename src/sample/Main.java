b3868dcb7dc97c7d4d0f81b7da7d7c435a55bcce
Maxim <githubmax@ukr.net>
2020-03-17 12:38:21 +0200

    add comments

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,15 +8,19 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    //обязательное переопределение абстрактного метода start() и класса Application
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/sample.fxml"));
-        primaryStage.setTitle("My first JavaFX");
+        primaryStage.setTitle("My first JavaFX"); //установка надписи для Stage
+
+        //дальше вкладываем в Stage Scene с размерами 700 на 400, где root это корневой компонент
+        // нашего пользовательского интерфейса, остальные компоненты вложены в него.
         primaryStage.setScene(new Scene(root, 700, 400));
-        primaryStage.show();
+        primaryStage.show(); //запуск Stage ("primaryStage")
     }
 
-
+    //стандартный запуск JavaFX приложения
     public static void main(String[] args) {
         launch(args);
     }
